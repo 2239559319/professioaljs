a2368645ae8594f7f9e3076f02f3ad00301896ca
2239559319 <2017141461249@stu.scu.edu.cn>
2019-05-01 21:47:34 +0800

    转换方法

---
+++ 第5章/5.2Array/5.2.2.js
@@ -0,0 +1,8 @@
+//转换方法
+//valueOf()方法返回本身
+var colors=["red","white","blue"];
+console.log(colors.valueOf());      //[ 'red', 'white', 'blue' ]
+//toString方法返回每一项调动toString方法再用,连接的字符串
+console.log(colors.toString());     //red,white,blue
+//toLocaleString()方法和toString原理一样
+console.log(colors.toLocaleString());   //red,white,blue
