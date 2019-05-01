8d097bf48a61a829ddf9834af80a1d7921a15cbc
2239559319 <2017141461249@stu.scu.edu.cn>
2019-05-01 21:36:52 +0800

    Array概述

--- 第5章/5.2Array/5.2.js
+++ 第5章/5.2Array/5.2.js
@@ -4,6 +4,8 @@
 var colors=new Array();
 var colors=new Array(20);       //数组长度为20
 var colors=new Array("red","green","blue");
+//字面量
+var colors=["red","green","blue"];
 
 colors[2]="black";      //修改第3项
 colors[3]="brown";      //新增第4项
